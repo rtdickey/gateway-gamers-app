ec9775262b98f88966ca84eb45a88fd781bc2040
Ryan Dickey <ryantay87@gmail.com>
2024-08-30 13:45:31 -0500

    Added navigation and basic story component.

---
+++ src/components/Nav/Nav.stories.tsx
@@ -0,0 +1,14 @@
+import { Meta, StoryObj } from "@storybook/react/*"
+
+import Nav from "./Nav"
+
+const meta: Meta<typeof Nav> = {
+  component: Nav,
+  title: "components/Nav",
+  tags: ["autodocs"],
+}
+
+export default meta
+type Story = StoryObj<typeof Nav>
+
+export const Default: Story = {}
