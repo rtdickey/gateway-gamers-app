15b9c147fe7032f8aa35a98b0f088be636022951
Ryan Dickey <ryantay87@gmail.com>
2024-09-17 20:41:20 -0500

    added delete

--- src/components/GameKeep/Shelf/Shelf.tsx
+++ src/components/GameKeep/Shelf/Shelf.tsx
@@ -1,9 +1,12 @@
-import React from "react"
+import React, { useCallback } from "react"
 
+import { faTrash } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { skipToken } from "@reduxjs/toolkit/query"
 
+import Button from "components/Button"
 import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "components/Table"
-import { useGetUserGamesQuery } from "services/userGamesApi"
+import { useGetUserGamesQuery, useDeleteUserGameMutation } from "services/userGamesApi"
 import { Game } from "types"
 
 interface ShelfProps {
@@ -12,6 +15,16 @@ interface ShelfProps {
 
 const Shelf: React.FC<ShelfProps> = ({ shelfId }) => {
   const { data: games } = useGetUserGamesQuery(shelfId.length ? shelfId : skipToken)
+  const [deleteGameApi] = useDeleteUserGameMutation()
+
+  const handleDelete = useCallback(
+    async (gameId: string) => {
+      if (window.confirm("Are you sure you want to delete this game?")) {
+        await deleteGameApi({ gameId, shelfId: parseInt(shelfId) })
+      }
+    },
+    [deleteGameApi, shelfId],
+  )
 
   return (
     <div className='flex-1 flex-col'>
@@ -26,6 +39,7 @@ const Shelf: React.FC<ShelfProps> = ({ shelfId }) => {
               <TableHead>Players</TableHead>
               <TableHead>Age</TableHead>
               <TableHead>Playing Time</TableHead>
+              <TableHead>Actions</TableHead>
             </TableRow>
           </TableHeader>
           <TableBody>
@@ -34,7 +48,15 @@ const Shelf: React.FC<ShelfProps> = ({ shelfId }) => {
               return (
                 <TableRow key={gameInfo.bgg_game_id}>
                   <TableCell className='flex items-center justify-center'>
-                    {gameInfo.thumbnail && <img src={gameInfo.thumbnail} alt={gameInfo.name} className='w-auto' />}
+                    {gameInfo.thumbnail && (
+                      <img
+                        src={gameInfo.thumbnail}
+                        alt={gameInfo.name}
+                        className='rounded-md min-w-20 min-h-20'
+                        height={80}
+                        width={80}
+                      />
+                    )}
                   </TableCell>
                   <TableCell>{gameInfo.bgg_game_id}</TableCell>
                   <TableCell>{gameInfo.name}</TableCell>
@@ -44,6 +66,11 @@ const Shelf: React.FC<ShelfProps> = ({ shelfId }) => {
                   </TableCell>
                   <TableCell>{gameInfo.age}+</TableCell>
                   <TableCell>{gameInfo.playing_time}m</TableCell>
+                  <TableCell>
+                    <Button variant='destructive' onClick={() => handleDelete(game.game_id)}>
+                      <FontAwesomeIcon icon={faTrash} size='sm' />
+                    </Button>
+                  </TableCell>
                 </TableRow>
               )
             })}
--- src/services/userGamesApi.ts
+++ src/services/userGamesApi.ts
@@ -6,6 +6,11 @@ import { UserGame } from "types"
 
 import { supabaseBaseQuery } from "./supabaseBaseQuery"
 
+interface DeleteUserGameRequest {
+  gameId: string
+  shelfId: number
+}
+
 interface AddUserGameRequest {
   gameId: string
   shelfId: number
@@ -55,9 +60,18 @@ const userGamesApi = createApi({
         const { data, error } = await supabase
           .from("UserGames")
           .upsert({ shelf_id: shelfId, game_id: gameId, user_id: userId })
-        // .select(
-        //   "id, game_id, Games (id, name, year_published, age, playing_time, min_players, max_players, bgg_game_id, thumbnail, image)",
-        // )
+
+        if (error) {
+          return { error }
+        }
+
+        return { data }
+      },
+      invalidatesTags: ["UserGames"],
+    }),
+    deleteUserGame: builder.mutation<PostgrestError | null, DeleteUserGameRequest>({
+      queryFn: async ({ gameId, shelfId }) => {
+        const { data, error } = await supabase.from("UserGames").delete().eq("game_id", gameId).eq("shelf_id", shelfId)
 
         if (error) {
           return { error }
@@ -70,5 +84,6 @@ const userGamesApi = createApi({
   }),
 })
 
-export const { useGetAllUserGamesQuery, useGetUserGamesQuery, useAddUserGameMutation } = userGamesApi
+export const { useGetAllUserGamesQuery, useGetUserGamesQuery, useAddUserGameMutation, useDeleteUserGameMutation } =
+  userGamesApi
 export { userGamesApi }
